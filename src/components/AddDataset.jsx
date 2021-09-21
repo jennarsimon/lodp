7deb9d17902f38ee9923ca611b635fd4712291d7
jennarsimon <jrs222@lehigh.edu>
2021-09-21 14:29:59 -0400

    added google form for submitting dataset

--- src/components/AddDataset.jsx
+++ src/components/AddDataset.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export class AddDataset extends React.Component {
     render() {
-        return <h1>Add Dataset</h1>;
+        return <iframe title="contribute-dataset" id="google-form" src="https://docs.google.com/forms/d/e/1FAIpQLSf1x5NMz1zK1SYPpuxjWNPBW_l3dgHA7F-hzykUdjQ-1rUVlQ/viewform?embedded=true" 
+        width="800" height="1653" frameborder="0" marginheight="0" marginwidth="0">
+            Loading…
+        </iframe>
     }
 }
