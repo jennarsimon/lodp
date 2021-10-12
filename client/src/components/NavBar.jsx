43bea898a48d9814f589553d9df69d82b0de225a
jennarsimon <jrs222@lehigh.edu>
2021-10-12 11:46:10 -0400

    added related links

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../index.css';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default class NavBar extends React.Component {
 
@@ -16,6 +17,11 @@ export default class NavBar extends React.Component {
                 <ul>
                     <li id="link"><a className="brown-text" href="/about-us">About Us</a></li>
                     <li id="link"><a className="brown-text" href="/contribute">Contribute a Dataset</a></li>
+                    <li>
+                    <NavDropdown id="link" className="brown-text" title="Related Links ▼">
+                            <NavDropdown.Item id="dropdown" className="brown-text" href="https://opendatacampus.com/" target="_blank">Open Data Handbook</NavDropdown.Item>
+                    </NavDropdown>
+                    </li>
                 </ul>
                 </div>
                 {/* <div className='line dark-brown'></div> */}
