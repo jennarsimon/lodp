b489d0efb630ef432f8be82097cb9dbaf64cc206
Adrian Ross <amr424@lehigh.edu>
2021-11-02 01:35:16 -0400

    Add tempData to incorporate JSON files with DatasetCard()

--- src/components/Dataset.jsx
+++ src/components/Dataset.jsx
@@ -4,19 +4,22 @@ import {
     Badge,
     ListGroup
 } from 'react-bootstrap';
+import data from '../testData/datasets.json';
 
 function DatasetCard(){
     return(
-        <Card style={{border:0}}>
+        data.map((dataset, idx) => (
+        <Card style={{border:0}}>  
             <Card.Body>
-                <Card.Title>Dataset Card</Card.Title>
-                <Card.Text>This is some text within a card body.</Card.Text>
+                <Card.Title>{dataset.title}</Card.Title>
+                <Card.Text>{dataset.desc}</Card.Text>
                 <Card.Subtitle className="mb-2 text-muted">
-                    Category: <Badge bg="dark">Category</Badge>{' '} |
-                    Filetype: <Badge bg="secondary">Filetype</Badge>
+                    Category: <Badge bg="dark">{dataset.category}</Badge>{' '} |
+                    Filetype: <Badge bg="secondary">{dataset.filetype}</Badge>
                 </Card.Subtitle>
             </Card.Body>
         </Card>
+        ))
     );
 }
 
