21e3b1c28de47cb7af8b187123f30ef64c4015ed
abigailvogel <89586378+abigailvogel@users.noreply.github.com>
2021-11-14 10:52:35 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,25 @@ export default class Navbar extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-dark bg-dark navgar-expand-lg">
-                <Link to="/" className="navbar-brand">Dataset List</Link>
-            </nav>
+            <div id="nav">
+                <div className='navbar'>
+                <div>
+                    <a href="/">
+                        <img alt='LOPD' src={logo} />
+                    </a>
+                </div>
+                <ul>
+                    <li id="link"><a className="brown-text" href="/aboutus">About Us</a></li>
+                    <li id="link"><a className="brown-text" href="/contribute">Contribute a Dataset</a></li>
+                    <li>
+                    <NavDropdown id="link" className="brown-text" title="Related Links ▼">
+                            <NavDropdown.Item id="dropdown" className="brown-text" href="https://opendatacampus.com/" target="_blank">Open Data Handbook</NavDropdown.Item>
+                    </NavDropdown>
+                    </li>
+                </ul>
+                </div>
+                {/* <div className='line dark-brown'></div> */}
+            </div>
         );
     }
 
