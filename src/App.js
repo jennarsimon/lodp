e0bd1d59dc2c52c329d2ddaca7a12c9127dd9315
jennarsimon <jrs222@lehigh.edu>
2021-12-25 15:13:23 -0600

    fixed banner image scrolling issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import banner from './assets/images/banner.jpg';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 import DatasetList from './components/DatasetList.jsx';
@@ -9,6 +10,7 @@ function App() {
   return (
     <Router>
       <div className="container">
+        { window.location.pathname === '/' && <img className='banner_img' alt='linderman rotunda' src={banner} /> }
         <Navbar />
         <Route path="/" exact component={DatasetList} />
         <Footer />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,22 +10,21 @@ export default class Navbar extends Component {
 
         return (
             <div id="nav">
-                { window.location.pathname === '/' && <img className='banner_img' alt='linderman rotunda' src={banner} /> }
                 <div className='navbar'>
-                <div>
-                    <a href="/">
-                        <img className="logo" alt='LOPD' src={logo} />
-                    </a>
-                </div>
-                <ul>
-                    <li id="link"><a className="brown-text" href="/aboutus">About Us</a></li>
-                    <li id="link"><a className="brown-text" href="/contribute">Contribute a Dataset</a></li>
-                    <li>
-                    <NavDropdown id="link" className="brown-text" title="Related Links">
-                            <NavDropdown.Item id="dropdown" className="brown-text" href="https://opendatacampus.com/" target="_blank">Open Data Handbook</NavDropdown.Item>
-                    </NavDropdown>
-                    </li>
-                </ul>
+                    <div>
+                        <a href="/">
+                            <img className="logo" alt='LOPD' src={logo} />
+                        </a>
+                    </div>
+                    <ul>
+                        <li id="link"><a className="brown-text" href="/aboutus">About Us</a></li>
+                        <li id="link"><a className="brown-text" href="/contribute">Contribute a Dataset</a></li>
+                        <li>
+                        <NavDropdown id="link" className="brown-text" title="Related Links">
+                                <NavDropdown.Item id="dropdown" className="brown-text" href="https://opendatacampus.com/" target="_blank">Open Data Handbook</NavDropdown.Item>
+                        </NavDropdown>
+                        </li>
+                    </ul>
                 </div>
             </div>
         );
