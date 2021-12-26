3981888ec289d7329391cf79c24022091e669e0b
jennarsimon <jrs222@lehigh.edu>
2021-12-26 12:22:40 -0600

    updated about page and contribute page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import banner from './assets/images/banner.jpg';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
-/* import DatasetList from './components/DatasetList.jsx'; */
 import SearchPage from './components/SearchPage.jsx';
+import AboutUs from './components/AboutUs.jsx';
+import AddDataset from './components/AddDataset.jsx';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         { window.location.pathname === '/' && <img className='banner_img' alt='linderman rotunda' src={banner} /> }
         <Navbar />
         <Route path="/" exact component={SearchPage} />
+        <Route path="/aboutus" exact component={AboutUs} />
+        <Route path="/contribute" exact component={AddDataset} />
         <Footer />
       </div>
     </Router>
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import jenna from '../assets/images/jenna.jpeg';
+import daniella from '../assets/images/daniella.jpeg';
+import hannah from '../assets/images/hannah.jpeg';
+import papa from '../assets/images/papa.jpeg';
+import jisoo from '../assets/images/jisoo.jpeg';
+import adrian from '../assets/images/adrian.png';
+import carl from '../assets/images/carl.jpeg';
+import abigail from '../assets/images/abigail.jpeg';
+import '../index.css';
+
+export default class AboutUs extends React.Component {
+    render() {
+        return (
+            <div id='aboutDiv'>
+                <div id="description" className="pale-brown">
+                    <div>
+                        <h1 id="aboutUsTitle">About Us</h1>
+                    </div>
+                    <div id="aboutText">
+                        <p>The Brown and White Tech and Innovation team is a Lehigh University student organization centered around open data, which is data that everyone can access. Our mission is to encourage data and information transparency at Lehigh. These datasets provide information that ranges from university tuition to student life statistics. We also collect data by acquiring statistics from Lehigh students, and compile and analyze the data so that it can be easily understood by the general public. This student-run group works with Lehigh University’s journalism department and The Brown and White to ensure that all of the data published are accurate and accessible.</p>
+                    </div>
+                </div>
+                <div id="team" className="brown-text">
+                    <h1 id="meetTeam">Meet the Team</h1>
+                    <div id="directory">
+                        <div id="jenna">
+                            <img alt="jenna" src={jenna} />
+                            <h2>Jenna Simon</h2>
+                            <h3>Class of 2022</h3>
+                        </div>
+                        <div id="hannah">
+                            <img alt="hannah" src={hannah} />
+                            <h2>Hannah Leland</h2>
+                            <h3>Class of 2022</h3>
+                        </div>
+                        <div id="jisoo">
+                            <img alt="jisoo" src={jisoo} />
+                            <h2>JiSoo Park</h2>
+                            <h3>Class of 2023</h3>
+                        </div>
+                        <div id="papa">
+                            <img alt="papa" src={papa} />
+                            <h2>Papa Asibuo</h2>
+                            <h3>Class of 2024</h3>
+                        </div>
+                        <div id="adrian">
+                            <img alt="adrian" src={adrian} />
+                            <h2>Adrian Ross</h2>
+                            <h3>Class of 2024</h3>
+                        </div>
+                        <div id="carl">
+                            <img alt="carl" src={carl} />
+                            <h2>Carl Freyer</h2>
+                            <h3>Class of 2025</h3>
+                        </div>
+                        <div id="daniella">
+                            <img alt="daniella" src={daniella} />
+                            <h2>Daniella Stokic</h2>
+                            <h3>Class of 2025</h3>
+                        </div>
+                        <div id="abigail">
+                            <img alt="abigail" src={abigail} />
+                            <h2>Abigail Vogel</h2>
+                            <h3>Class of 2025</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
