0fc9633c968b00b8922351ec3c4f2996977f4f1c
jennarsimon <jrs222@lehigh.edu>
2021-12-31 12:19:44 -0600

    moving to master

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from '../assets/images/lodp.png';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import '../index.css';
+
+export default class Navbar extends Component {
+
+    render() {
+
+        return (
+            <div id="nav">
+                <div className='navbar'>
+                    <div>
+                        <a href="/">
+                            <img className="logo" alt='LOPD' src={logo} />
+                        </a>
+                    </div>
+                    <ul>
+                        <li id="link"><a className="brown-text" href="/aboutus">About Us</a></li>
+                        <li id="link"><a className="brown-text" href="/contribute">Contribute a Dataset</a></li>
+                        <li>
+                        <NavDropdown id="link" className="brown-text" title="Related Links">
+                                <NavDropdown.Item id="dropdown" className="brown-text" href="https://opendatacampus.com/" target="_blank">Open Data Handbook</NavDropdown.Item>
+                        </NavDropdown>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+
+}
